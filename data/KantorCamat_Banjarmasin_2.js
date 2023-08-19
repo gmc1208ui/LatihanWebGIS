306cb9e6119185edcf37be838c61897588bb6270
gmc1208ui <142674536+gmc1208ui@users.noreply.github.com>
2023-08-19 20:58:23 +0700

    Add files via upload

---
+++ data/KantorCamat_Banjarmasin_2.js
@@ -0,0 +1,11 @@
+var json_KantorCamat_Banjarmasin_2 = {
+"type": "FeatureCollection",
+"name": "KantorCamat_Banjarmasin_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 40302.0, "FCODE": "64112", "NAMOBJ": "Banjar Selatan", "ALIAS": null, "LUAS": 0.0, "SJRNAM": null, "LOKTPN": null, "REMARK": "Ibukota Kecamatan", "KLSTPN": null, "SRS_ID": null }, "geometry": { "type": "Point", "coordinates": [ 114.591272515000014, -3.330292791999966 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 40303.0, "FCODE": "64112", "NAMOBJ": "Banjar Timur", "ALIAS": null, "LUAS": 0.0, "SJRNAM": null, "LOKTPN": null, "REMARK": "Ibukota Kecamatan", "KLSTPN": null, "SRS_ID": null }, "geometry": { "type": "Point", "coordinates": [ 114.610997133000069, -3.323869009999953 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 40304.0, "FCODE": "64112", "NAMOBJ": "Banjar Barat", "ALIAS": null, "LUAS": 0.0, "SJRNAM": null, "LOKTPN": null, "REMARK": "Ibukota Kecamatan", "KLSTPN": null, "SRS_ID": null }, "geometry": { "type": "Point", "coordinates": [ 114.587029583000117, -3.32190413099994 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 40306.0, "FCODE": "64112", "NAMOBJ": "Banjar Utara", "ALIAS": null, "LUAS": 0.0, "SJRNAM": null, "LOKTPN": null, "REMARK": "Ibukota Kecamatan", "KLSTPN": null, "SRS_ID": null }, "geometry": { "type": "Point", "coordinates": [ 114.595875945000103, -3.308460676999971 ] } }
+]
+}
